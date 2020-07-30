3f9a8b999a042516fcc7cb953c6f887d1a10c831
Gurvinder Singh <gurvsin3@in.ibm.com>
2020-07-30 15:27:48 +0530

    first commit

---
+++ services/edge-gateway/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {EdgeGatewayApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new EdgeGatewayApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: EdgeGatewayApplication;
+  client: Client;
+}
