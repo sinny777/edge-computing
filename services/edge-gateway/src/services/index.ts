9b9c394eb9256c521366365f83db56000a83894f
Gurvinder Singh <gurvsin3@in.ibm.com>
2021-04-20 14:31:49 +0530

    removed unnecessary files from git

--- services/edge-gateway/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './common.service';
-export * from './radio.service';
-export * from './rule.service';
-export * from './gateway.service';
-
--- services/myhelloworld/service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Very simple Horizon sample edge service.
-
-while true; do
-  echo "$HZN_DEVICE_ID says: Hey there ${HW_WHO}!"
-  sleep 3
-done
