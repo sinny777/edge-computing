154c8652a5047a1c7e431b91bdc498325c18698f
Gurvinder Singh <gurvsin3@in.ibm.com>
2021-06-10 11:13:19 +0530

    Small changes in Git workflow for arm32v6

--- services/edge-gateway/docker_build.sh
+++ services/edge-gateway/docker_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export APP_VERSION=$(grep -oE "\"version\": \"(\w*.\w*.\w*.\w*.\w*.)" app/package.json | cut -d\" -f4)
-export ARCH=arm64
+export ARCH=arm32v6
+export NODE_VERSION=12.22.1
+export OS=alpine
 
 echo "#########################################################################"
 echo "Build EdgeGateway Service version: ${APP_VERSION} for ${ARCH}"
@@ -11,12 +13,15 @@ echo "#########################################################################"
 #   --platform=linux/arm64 .
 
 sudo docker buildx build --rm  \
-    --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
-    --build-arg NODE_VERSION=12.22.1 \
-    --build-arg TAG_SUFFIX=${APP_VERSION} \
-    --file Dockerfile \
-    --tag sinny777/edge-gateway_${ARCH}:${APP_VERSION} \
-    --platform=linux/${ARCH} --push .
+    --build-arg ARCH=${ARCH} \
+    --build-arg VERSION=${APP_VERSION} \
+    --build-arg NODE_VERSION=${NODE_VERSION} \
+    --build-arg OS=${OS} \
+    --build-arg TAG_SUFFIX=${TAG_SUFFIX} \
+    --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
+    --file Dockerfile.alpine \
+    --tag sinny777/edge-gateway_arm:${APP_VERSION} \
+    --platform=linux/arm/v6,linux/arm/v7 --push .
 
 # sudo docker build --rm  \
 #     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
